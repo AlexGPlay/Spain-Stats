c8e0a9cbdc9c9df0819327ca3284c68a4e1e88a5
AlexGPlay <alexgp1234@gmail.com>
2023-11-09 21:13:18 +0100

    Update existing geojsons and app

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,41 @@
+import { useState } from "react";
 import Map from "./Map";
 import { Card, CardContent } from "./components/Card";
 import Flex from "./components/Flex";
 import Join from "./components/Join";
+import LayerSearch from "./components/LayerSearch";
 import Link from "./components/Link";
 import useGeojson, { usePrefetchGeojson } from "./hooks/useGeojson";
 import { useSelectedLayer } from "./hooks/useSelectedLayer";
-import { DoubleArrowRightIcon, UpdateIcon } from "@radix-ui/react-icons";
+import {
+  DoubleArrowRightIcon,
+  MagnifyingGlassIcon,
+  UpdateIcon,
+} from "@radix-ui/react-icons";
 
 function App() {
   const [selectedLayer, setSelectedLayer] = useSelectedLayer();
+  const [isLayerSearchOpen, setIsLayerSearchOpen] = useState(false);
 
   const { isLoading: isGeojsonLoading, data: geojsonData } =
     useGeojson(selectedLayer);
   const { prefetchGeojson } = usePrefetchGeojson();
 
   const breadcrumbs = [
-    {
-      name: geojsonData?.properties.parent_name,
-      id: geojsonData?.properties.parent_id,
-    },
+    ...(geojsonData?.properties.parents || []),
     { name: geojsonData?.properties.name, id: geojsonData?.properties.id },
-  ].filter((elem) => elem.id !== undefined && elem.id !== null);
+  ];
 
   return (
     <div className="h-screen w-full relative">
+      <LayerSearch
+        isOpen={isLayerSearchOpen}
+        onOpenChange={(isOpen) => setIsLayerSearchOpen(isOpen)}
+        onLayerSelected={(layerId) => {
+          setIsLayerSearchOpen(false);
+          setSelectedLayer(layerId);
+        }}
+      />
       <Map
         fitBoundsOnChange
         layer={geojsonData}
@@ -33,35 +45,43 @@ function App() {
         onFeatureHover={({ feature }) => prefetchGeojson(feature.properties.id)}
       />
       <div className="h-full w-[350px] absolute z-10 p-5 pointer-events-none">
-        <Card className="h-fit w-fit pointer-events-auto">
-          <CardContent className="p-1 pl-2 pr-2">
-            <Flex className="items-center">
-              {isGeojsonLoading ? (
-                <div className="animate-spin text-gray-200">
-                  <UpdateIcon />
-                </div>
-              ) : (
-                <Join
-                  separator={
-                    <div className="text-xs text-gray-200 pr-2 pl-2">
-                      <DoubleArrowRightIcon />
-                    </div>
-                  }
-                >
-                  {breadcrumbs.map(({ name, id }) => (
-                    <Link
-                      key={id}
-                      className="text-xs text-gray-200"
-                      onClick={() => setSelectedLayer(id!)}
-                    >
-                      {name}
-                    </Link>
-                  ))}
-                </Join>
-              )}
-            </Flex>
-          </CardContent>
-        </Card>
+        <Flex className="gap-3">
+          <Card
+            onClick={() => setIsLayerSearchOpen(true)}
+            className="flex justify-center items-center pl-2 pr-2 cursor-pointer pointer-events-auto"
+          >
+            <MagnifyingGlassIcon />
+          </Card>
+          <Card className="h-fit w-fit pointer-events-auto">
+            <CardContent className="p-1 pl-2 pr-2">
+              <Flex className="items-center">
+                {isGeojsonLoading ? (
+                  <div className="animate-spin text-gray-200">
+                    <UpdateIcon />
+                  </div>
+                ) : (
+                  <Join
+                    separator={
+                      <div className="text-xs text-gray-200 pr-2 pl-2">
+                        <DoubleArrowRightIcon />
+                      </div>
+                    }
+                  >
+                    {breadcrumbs.map(({ name, id }) => (
+                      <Link
+                        key={id}
+                        className="text-xs text-gray-200"
+                        onClick={() => setSelectedLayer(id!)}
+                      >
+                        {name}
+                      </Link>
+                    ))}
+                  </Join>
+                )}
+              </Flex>
+            </CardContent>
+          </Card>
+        </Flex>
       </div>
     </div>
   );
